a2a7cfd75a60063869cda50a0c6853be8bb85af5
Martin <martin@GF426MHJ7P.local>
2025-01-06 17:16:58 +0100

    create classes files

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Game from "./Game"
 
 const App = () => {
-  return <h1>Hey Mate !</h1>;
+  return <>
+    <h1>Hey Mate !</h1>
+    <Game />
+  </>;
 };
 
 export default App;
