c9f84ffa202d28eb3983b639820a0c57bd934d49
Martin <martin@GF426MHJ7P.local>
2025-04-03 17:30:11 +0200

    Insert monster in grid matrix

--- app/src/Game.js
+++ app/src/Game.js
@@ -3,16 +3,18 @@ import Grid from "./Grid"
 
 export const IndexContext = createContext(null);
 
+
 export default function Game(){
 
-    const [indexClick, setIndexClick] = useState();
+    const [indexClicked, setIndexClicked] = useState();
 
     return (
         <div>
-            <IndexContext.Provider value={[indexClick, setIndexClick]}>
-                <Grid colSize={8} rowSize={8}/>
-            </IndexContext.Provider>
-            <p>You clicked on: {indexClick}</p>
+                <IndexContext.Provider value={[indexClicked, setIndexClicked]}>
+                    <Grid colSize={8} rowSize={8}/>
+                </IndexContext.Provider>
+
+            <p>You clicked on: {indexClicked}</p>
         </div>
    );
 }
--- app/src/Grid.js
+++ app/src/Grid.js
@@ -1,6 +1,8 @@
-import React, {useState, useContext} from "react"
+import React, {useState, useContext, createContext, useEffect} from "react"
 import Cell from "./Cell"
 import { IndexContext } from "./Game";
+
+export const GridContext = createContext([]);
 
 export default function Grid(props){
 
@@ -9,29 +11,59 @@ export default function Grid(props){
         colSize: props.colSize
     };
 
-    const [indexClick, setIndexClick] = useContext(IndexContext);
-
-    // console.log(indexClick);
-    let rows = [];
-    for(let i = 1; i < gridSize.rowSize + 1; i++){
-        rows.push(i);
-    };
+    const [indexClicked, setIndexClicked] = useContext(IndexContext);
+    const [gridContent, setGridContent] = useState(GridContext);
 
     let cols = [];
+    
     for(let y = 1; y < gridSize.colSize + 1; y++){
         cols.push(y);
     };
 
+    let rows = [];
+    for(let i = 1; i < gridSize.rowSize + 1; i++){
+        rows.push(i);
+
+    };
+
+    useEffect(() => {
+        let grid = [];
+
+        for(let z = 0; z < rows.length; z++){
+            let cellsContent = [];
+            for (let a = 0; a < cols.length; a++){
+                cellsContent.push(null);
+            }
+            grid.push(cellsContent);
+            console.log(grid);
+        }
+        setGridContent(grid);
+    }, [gridSize.rowSize, gridSize.colSize]);
+
+    function handleClick(col, row, gridContent){
+        console.log(gridContent);
+        const nextGridContent = [...gridContent];
+        console.log("nextGridContent:");
+        console.log(nextGridContent);
+        nextGridContent[row-1][col-1] = 1;
+        setGridContent(nextGridContent);
+        console.log("gridContent:");
+        console.log(gridContent);
+    };
+        console.log(gridContent);
+
     return (<div className = "">
-        {rows.map((row) => (
-            <div className = "margin-col grid-col" key={row}>
-                {cols.map((col) => (
-                    <div className="margin-cell size-cell fill-yellow d-flex" key={col + row} onClick = {() => setIndexClick(col.toString()+"-"+row.toString())}>
-                        <Cell rowIndex={row} colIndex={col}/>
-                    </div>
-                ))}
-            </div>
-        ))}
+        <GridContext.Provider value={[gridContent, setGridContent]}>
+            {rows.map((row) => (
+                <div className = "margin-col grid-col" key={row}>
+                    {cols.map((col) => (
+                        <div className="margin-cell size-cell fill-yellow d-flex" key={col + row} onClick = {() => handleClick(col, row, gridContent)}>
+                            <Cell rowIndex={row} colIndex={col}/>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </GridContext.Provider>
     </div>
     )
 }
